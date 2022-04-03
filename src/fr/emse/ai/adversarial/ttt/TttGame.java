294a56ffff2d429a90e3b555c826c29688248bce
Elouan <elouanderenne@gmail.com>
2022-04-03 14:02:01 +0200

    pour ugo j'ai le utility

--- src/fr/emse/ai/adversarial/ttt/TttGame.java
+++ src/fr/emse/ai/adversarial/ttt/TttGame.java
@@ -73,6 +73,13 @@ public class TttGame implements Game<List<List<Integer>>, List<Integer>, Integer
     }
 
     private Integer winner(List<List<Integer>> state){
+        // return the winner of the game
+        for (int i =0; i<3;i++) {
+            if(state.get(i).get(0)==state.get(i).get(1) && state.get(i).get(0)==state.get(i).get(2)) return  state.get(i).get(0); // win with a line
+            if(state.get(0).get(i)==state.get(1).get(i) && state.get(0).get(i)==state.get(2).get(i)) return  state.get(0).get(i); // win with a column
+        }
+        if (state.get(0).get(0)==state.get(1).get(1) && state.get(0).get(0)==state.get(2).get(2)) return state.get(1).get(1); // win with diagonal
+        if (state.get(0).get(2)==state.get(1).get(1) && state.get(0).get(0)==state.get(2).get(0)) return state.get(1).get(1); // win with the other diagonal
         return null;
     }
 
@@ -85,7 +92,11 @@ public class TttGame implements Game<List<List<Integer>>, List<Integer>, Integer
     }
 
     @Override
-    public double getUtility(List<List<Integer>> lists, Integer integer) {
+    public double getUtility(List<List<Integer>> state, Integer player) {
+        // Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
+        player = winner(state);
+        if (player == 1) return 1;
+        if (player == 0) return -1;
         return 0;
     }
 }
